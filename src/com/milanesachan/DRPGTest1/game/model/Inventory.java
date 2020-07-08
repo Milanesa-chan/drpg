577b31a9d332d21e57fcdc675b038fe6f70c83b5
Milanesa-chan <lucagutierrez@hotmail.com>
2020-07-08 17:28:23 -0300

    Created 'UserItem' and got started the database related methods in 'Inventory'

--- src/com/milanesachan/DRPGTest1/game/model/Inventory.java
+++ src/com/milanesachan/DRPGTest1/game/model/Inventory.java
@@ -1,7 +1,13 @@
 package com.milanesachan.DRPGTest1.game.model;
 
+import com.milanesachan.DRPGTest1.bot.core.ItemFactory;
 import com.milanesachan.DRPGTest1.commons.exceptions.CharacterNotFoundException;
+import com.milanesachan.DRPGTest1.networking.DatabaseConnector;
 
+import java.sql.*;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Inventory extends ArrayList<UserItem> {
@@ -12,11 +18,41 @@ public class Inventory extends ArrayList<UserItem> {
         userID = uid;
     }
 
-    public void saveToDatabase(){
-
+    public void saveToDatabase() throws SQLException {
+        if(!super.isEmpty()){
+            Connection con = DatabaseConnector.getInstance().getDatabaseConnection();
+            if(con != null) {
+                for (UserItem ui : this) {
+                    PreparedStatement stmt = con.prepareStatement("INSERT INTO `inventory` (UID, ItemID, Quantity, DateObtained) VALUES (?, ?, ?, ?)" +
+                            "ON DUPLICATE KEY UPDATE Quantity = Quantity + 1;");
+                    stmt.setLong(1, userID);
+                    stmt.setString(2, ui.getItem().getItemID());
+                    stmt.setInt(3, 1);
+                    stmt.setString(4, ui.getDateObtained().format(DateTimeFormatter.ISO_DATE_TIME));
+                }
+            }
+        }
     }
 
-    public void loadFromDatabase() throws CharacterNotFoundException {
+    public void loadFromDatabase() throws CharacterNotFoundException, SQLException {
+        Connection con = DatabaseConnector.getInstance().getDatabaseConnection();
+        if(con != null){
+            Statement stmt = con.createStatement();
+            ResultSet result = stmt.executeQuery("SELECT * FROM `inventory` WHERE `UID`="+userID+";");
+            this.clear();
+            while(result.next()){
+                UserItem ui = new UserItem();
+                ui.setUserID(userID);
+                ui.setItem(ItemFactory.getInstance().getItemFromID(result.getString("ItemID")));
+                ui.setQuantity(result.getInt("Quantity"));
+                OffsetDateTime dateTime = result.getTimestamp("DateObtained").toInstant().atOffset(ZoneOffset.ofHours(-3));
+                ui.setDateObtained(dateTime);
+                this.add(ui);
+            }
+        }
+    }
+
+    public void sortByDate(){
         
     }
 
--- src/com/milanesachan/DRPGTest1/game/model/UserItem.java
+++ src/com/milanesachan/DRPGTest1/game/model/UserItem.java
@@ -1,5 +1,52 @@
 package com.milanesachan.DRPGTest1.game.model;
 
-public class UserItem {
+import java.time.OffsetDateTime;
 
+public class UserItem {
+    private Item item;
+    private long userID;
+    private OffsetDateTime dateObtained;
+    private int quantity;
+
+    public UserItem() {}
+
+    public UserItem(Item item, long userID, OffsetDateTime dateObtained) {
+        super();
+        this.item = item;
+        this.userID = userID;
+        this.dateObtained = dateObtained;
+        quantity = 1;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public OffsetDateTime getDateObtained() {
+        return dateObtained;
+    }
+
+    public void setDateObtained(OffsetDateTime dateObtained) {
+        this.dateObtained = dateObtained;
+    }
 }
