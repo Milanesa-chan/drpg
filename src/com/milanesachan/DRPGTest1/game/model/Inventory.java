859d7164e5eae1ff614a5163af27ce5bbc98f1f6
Milanesa-chan <lucagutierrez@hotmail.com>
2020-07-08 16:47:15 -0300

    Started Inventory class

--- src/com/milanesachan/DRPGTest1/game/model/Inventory.java
+++ src/com/milanesachan/DRPGTest1/game/model/Inventory.java
@@ -1,5 +1,30 @@
 package com.milanesachan.DRPGTest1.game.model;
 
-public class Inventory {
+import com.milanesachan.DRPGTest1.commons.exceptions.CharacterNotFoundException;
 
+import java.util.ArrayList;
+
+public class Inventory extends ArrayList<UserItem> {
+    private long userID;
+
+    public Inventory(long uid){
+        super();
+        userID = uid;
+    }
+
+    public void saveToDatabase(){
+
+    }
+
+    public void loadFromDatabase() throws CharacterNotFoundException {
+        
+    }
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
 }
