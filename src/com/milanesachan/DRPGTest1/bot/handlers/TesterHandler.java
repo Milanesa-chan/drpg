c9c69bdc791102dd82423b49d928d9076e295df4
Milanesa-chan <lucagutierrez@hotmail.com>
2020-07-10 10:40:09 -0300

    Added handler class

---
+++ src/com/milanesachan/DRPGTest1/bot/handlers/TesterHandler.java
@@ -0,0 +1,17 @@
+package com.milanesachan.DRPGTest1.bot.handlers;
+
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+public class TesterHandler implements Handler{
+    private MessageChannel channel;
+
+    public TesterHandler(MessageChannel mc){
+        channel = mc;
+    }
+
+
+    @Override
+    public void handle() {
+        
+    }
+}
