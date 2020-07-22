497f76060fb8867f3ec138e39f06cd4881e7fbde
Milanesa-chan <lucagutierrez@hotmail.com>
2020-07-22 05:57:17 -0300

    Started the 'GuildParty' object

--- src/com/milanesachan/DRPGTest1/bot/entities/GuildParty.java
+++ src/com/milanesachan/DRPGTest1/bot/entities/GuildParty.java
@@ -1,5 +1,31 @@
 package com.milanesachan.DRPGTest1.bot.entities;
 
-public class GuildParty {
+import com.milanesachan.DRPGTest1.bot.handlers.guild.GuildDeletionHandler;
+import com.milanesachan.DRPGTest1.commons.exceptions.AlreadyInPartyException;
+import com.milanesachan.DRPGTest1.game.battle.BattleCharacter;
 
+import java.util.ArrayList;
+
+public class GuildParty {
+    private long guildID;
+    private ArrayList<BattleCharacter> charList;
+
+    public GuildParty(long guildID){
+        this.guildID = guildID;
+        charList = new ArrayList<>();
+    }
+
+    public long getGuildID() {
+        return guildID;
+    }
+
+    public void setGuildID(long guildID) {
+        this.guildID = guildID;
+    }
+
+    public void add(BattleCharacter character) throws AlreadyInPartyException {
+        if(charList.contains(character))
+            throw new AlreadyInPartyException(character, this);
+        else charList.add(character);
+    }
 }
