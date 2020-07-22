f7551f3c138174f81246b5ffb816184b7e6be835
Milanesa-chan <lucagutierrez@hotmail.com>
2020-07-22 04:55:13 -0300

    Improved the 'BattleCharacter' class

--- src/com/milanesachan/DRPGTest1/game/battle/BattleCharacter.java
+++ src/com/milanesachan/DRPGTest1/game/battle/BattleCharacter.java
@@ -1,14 +1,77 @@
 package com.milanesachan.DRPGTest1.game.battle;
 
+import com.milanesachan.DRPGTest1.commons.exceptions.CharacterNotFoundException;
 import com.milanesachan.DRPGTest1.game.model.Character;
+import com.milanesachan.DRPGTest1.game.model.Equipment;
 import com.milanesachan.DRPGTest1.game.model.items.Weapon;
 
-public class BattleCharacter {
-    private Character character;
-    private Weapon weapon;
+import java.sql.SQLException;
 
-    public BattleCharacter(Character character) {
+public class BattleCharacter {
+    private long userID;
+    private Character character;
+    private Equipment equipment;
+    private Weapon weapon;
+    private int HP, maxHP;
+
+    public BattleCharacter(long userID) throws SQLException, CharacterNotFoundException {
+        this.userID = userID;
+
+        character = new Character(userID);
+        character.loadFromDatabase();
+        maxHP = character.getHP();
+        HP = maxHP;
+
+        equipment = new Equipment(userID);
+        equipment.loadFromDatabase();
+        weapon = equipment.getWeapon();
+    }
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character character) {
         this.character = character;
-        //weapon = character.getEquipment().getWeapon();
+    }
+
+    public Equipment getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(Equipment equipment) {
+        this.equipment = equipment;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public void setMaxHP(int maxHP) {
+        this.maxHP = maxHP;
     }
 }
