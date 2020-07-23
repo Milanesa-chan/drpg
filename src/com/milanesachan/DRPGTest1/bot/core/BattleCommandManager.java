f0d2d1b10161d660536f90a4ea50e548d635cdf7
Milanesa-chan <lucagutierrez@hotmail.com>
2020-07-23 17:56:27 -0300

    Added check for battle channel in BattleCommandManager

--- src/com/milanesachan/DRPGTest1/bot/core/BattleCommandManager.java
+++ src/com/milanesachan/DRPGTest1/bot/core/BattleCommandManager.java
@@ -1,6 +1,7 @@
 package com.milanesachan.DRPGTest1.bot.core;
 
 import com.milanesachan.DRPGTest1.bot.entities.GuildParty;
+import com.milanesachan.DRPGTest1.bot.handlers.Handler;
 import com.milanesachan.DRPGTest1.bot.handlers.battle.CreatePartyHandler;
 import com.milanesachan.DRPGTest1.bot.handlers.battle.SetBattleChannelHandler;
 import com.milanesachan.DRPGTest1.commons.exceptions.ServerNotFoundException;
@@ -37,18 +38,44 @@ public class BattleCommandManager extends ListenerAdapter {
                     event.getMember().getUser().getIdLong(),
                     event.getGuild().getIdLong());
             h.handle();
-        }else if(matchCommand(args[0], "battle")){
+        }
+        if(matchCommand(args[0], "battle")){
             long guildID = event.getGuild().getIdLong();
             long userID = event.getMember().getUser().getIdLong();
-            new CreatePartyHandler(event.getChannel(), guildID, userID).handle();
+            CreatePartyHandler h = new CreatePartyHandler(event.getChannel(), guildID, userID);
+            onBattleChannelRequiredCommand(event, h);
         }
         //First check if the received message is a battle command
         //If it is, and it's not coming from a battle channel, print it
         //If it is coming from a battle channel, continue
     }
 
-    private boolean isBattleChannel(MessageReceivedEvent event, long guildID, long channelID){
+    private void onBattleChannelRequiredCommand(MessageReceivedEvent event, Handler h){
+        if(isBattleChannel(event)){
+            h.handle();
+        }else{
+            try {
+                Guild g = new GuildFactory().guildFromServerID(event.getGuild().getIdLong());
+                event.getChannel().sendMessage("This command is only for battle channels.").queue();
+                if(g.getBattleChannelID()==0) {
+                    event.getChannel().sendMessage("This server has no battle channel! Ask the owner to type" +
+                            " '>setbattlechannel' on a channel of this server.").queue();
+                }else{
+                    long channelID = g.getBattleChannelID();
+                    event.getChannel().sendMessage("This server's battle channel is: <#"+channelID+">").queue();
+                }
+            } catch (SQLException throwables) {
+                event.getChannel().sendMessage("Error connecting to the database.").queue();
+            } catch (ServerNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean isBattleChannel(MessageReceivedEvent event){
         try {
+            long guildID = event.getGuild().getIdLong();
+            long channelID = event.getChannel().getIdLong();
             Guild g = new GuildFactory().guildFromServerID(guildID);
             return g.getBattleChannelID()==channelID;
         } catch (SQLException throwables) {
