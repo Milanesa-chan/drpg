100b5477804badb3a9d5f2db569041c20a9c249f
Milanesa-chan <lucagutierrez@hotmail.com>
2020-07-23 17:45:54 -0300

    Added 'isBattleChannel' check in BattleCommandManager

--- src/com/milanesachan/DRPGTest1/bot/core/BattleCommandManager.java
+++ src/com/milanesachan/DRPGTest1/bot/core/BattleCommandManager.java
@@ -3,10 +3,13 @@ package com.milanesachan.DRPGTest1.bot.core;
 import com.milanesachan.DRPGTest1.bot.entities.GuildParty;
 import com.milanesachan.DRPGTest1.bot.handlers.battle.CreatePartyHandler;
 import com.milanesachan.DRPGTest1.bot.handlers.battle.SetBattleChannelHandler;
+import com.milanesachan.DRPGTest1.commons.exceptions.ServerNotFoundException;
+import com.milanesachan.DRPGTest1.game.model.Guild;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class BattleCommandManager extends ListenerAdapter {
@@ -42,6 +45,18 @@ public class BattleCommandManager extends ListenerAdapter {
         //First check if the received message is a battle command
         //If it is, and it's not coming from a battle channel, print it
         //If it is coming from a battle channel, continue
+    }
+
+    private boolean isBattleChannel(MessageReceivedEvent event, long guildID, long channelID){
+        try {
+            Guild g = new GuildFactory().guildFromServerID(guildID);
+            return g.getBattleChannelID()==channelID;
+        } catch (SQLException throwables) {
+            event.getChannel().sendMessage("Error connecting to database. Try again later.").queue();
+        } catch (ServerNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     private boolean matchCommand(String comm, String test) {
