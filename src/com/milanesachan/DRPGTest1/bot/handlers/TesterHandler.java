3964fe18ace41db9802f295146bbc2215c7deb16
Milanesa-chan <lucagutierrez@hotmail.com>
2020-09-03 09:50:20 -0300

    Added statcard drop to TesterHandler

--- src/com/milanesachan/DRPGTest1/bot/handlers/TesterHandler.java
+++ src/com/milanesachan/DRPGTest1/bot/handlers/TesterHandler.java
@@ -2,12 +2,17 @@ package com.milanesachan.DRPGTest1.bot.handlers;
 
 import com.milanesachan.DRPGTest1.bot.core.AnswerManager;
 import com.milanesachan.DRPGTest1.bot.core.DRPGBot;
+import com.milanesachan.DRPGTest1.bot.core.ItemFactory;
 import com.milanesachan.DRPGTest1.bot.handlers.statcards.FusionHandler;
 import com.milanesachan.DRPGTest1.bot.image_generator.TestImageGenerator;
+import com.milanesachan.DRPGTest1.commons.exceptions.ItemNotFoundException;
+import com.milanesachan.DRPGTest1.game.model.Inventory;
+import com.milanesachan.DRPGTest1.game.model.items.StatCard;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.awt.*;
+import java.sql.SQLException;
 
 public class TesterHandler implements Handler, Answerable{
     private MessageChannel channel;
@@ -20,7 +25,18 @@ public class TesterHandler implements Handler, Answerable{
 
     @Override
     public void handle() {
-
+        String[] args = event.getMessage().getContentRaw().split(" ");
+        if(args.length < 4){
+            channel.sendMessage("Format is: '>test amount stat_average stat_radius'").queue();
+        }else{
+            try {
+                generateRandomCards(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]), event.getMember().getIdLong());
+                channel.sendMessage("Generated cards.").queue();
+            } catch (SQLException | ItemNotFoundException throwables) {
+                channel.sendMessage("Unexpected error.").queue();
+                throwables.printStackTrace();
+            }
+        }
     }
 
     private boolean isInt(String s){
@@ -30,6 +46,23 @@ public class TesterHandler implements Handler, Answerable{
         }catch (NumberFormatException e){
             return false;
         }
+    }
+
+    private void generateRandomCards(int amount, int statAvg, int statRadius, long userID) throws SQLException, ItemNotFoundException {
+        Inventory userInv = new Inventory(userID).loadFromDatabase();
+        char[] colors = {'r', 'g', 'b'};
+        for(int c=0; c<amount; c++){
+            int[] stats = new int[4];
+            for(int s=0; s<4; s++){
+                stats[s] = (int) (statAvg - statRadius + Math.random()*statRadius*2);
+            }
+
+            char color = colors[(int)(Math.random()*3)];
+
+            StatCard newCard = ItemFactory.getInstance().getStatCard(color, stats[0], stats[1], stats[2], stats[3]);
+            userInv.add(newCard);
+        }
+        userInv.saveToDatabase();
     }
 
     @Override
